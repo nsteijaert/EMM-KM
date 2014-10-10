1faacc88abe0feadaf1bba659ea69f84f023a20b
nsteijaert <nsteijaert@ip248-158.stu-wireless.hzeeland.nl>
2014-10-10 11:53:59 +0200

    Multiple small comment and code edits

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,6 +1,11 @@
+//@author NJK
+//This script contains custom function used for utility purposes.
+
+//This method is used to increment the progressbar of the intro page
 function set_state(state,text)
 {
 	document.getElementById("bar_state").innerHTML = state;
 	document.getElementById("bar_state").setAttribute("style","width:"+ state + ";");
+	//document.getElementById("bar_state").setAttribute("aria-valuenow",state);
 	document.getElementById("state_info").innerHTML = text;
 }
